e6f1397a34b34c79b0654886a04123e8d224a961
davebulaval <david.beauchemin.5@ulaval.ca>
2023-02-16 06:59:03 -0500

    remove deprecated transformers.adamW for pytorch version

--- utils_optim.py
+++ utils_optim.py
@@ -1,4 +1,4 @@
-from transformers.optimization import AdamW
+from torch.optim.AdamW import AdamW
 from torch.optim.lr_scheduler import LambdaLR
 from torch.optim import Optimizer, SGD
 from torch import nn
