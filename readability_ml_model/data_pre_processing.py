492e07725979daedd6dedf85cd89c0465ea0b71d
davebulaval <david.beauchemin.5@ulaval.ca>
2023-03-15 08:31:09 -0400

    add class approach in training data

--- readability_ml_model/data_pre_processing.py
+++ readability_ml_model/data_pre_processing.py
@@ -15,6 +15,7 @@ for row_metadata in tqdm(metadata.iterrows(), total=len(metadata)):
     row_metadata_serie = row_metadata[1]
     if row_metadata_serie["language"] == "en":
         Y = row_metadata_serie["grade_level"]
+        Y_class = f"[{str(Y-1)}, {str(Y+1)}]"
         file_name = row_metadata_serie["filename"]
         with open(f"../datastore/articles/{file_name}", "r", encoding="utf8") as file:
             all_text = file.readlines()
@@ -23,11 +24,11 @@ for row_metadata in tqdm(metadata.iterrows(), total=len(metadata)):
             )  # We split paragraphs to the double newline character
             for paragraph_idx, paragraph in enumerate(paragraphs):
                 X = tcc.get_metrics_scores(paragraph)
-                data_to_append = [Y]
+                data_to_append = [Y, Y_class]
                 data_to_append.extend(X.iloc[0, :].tolist())
                 data.append(data_to_append)
 
-columns = ["Y"]
+columns = ["Y", "Y_class"]
 columns.extend(X.columns)
 pd.DataFrame(data, columns=columns).to_csv(
     os.path.join(root, "datastore", "pre_process_newsela_data.csv"), index=False
